5a473ef34720895c387e7211546810236645b514
RhuanDevOnnet <desenvolvimento01.onnetmais@gmail.com>
2023-11-03 16:38:09 -0300

    Exibir Cidade, Funcao, Etc corretos e filtros de demitidos feito

--- script.js
+++ script.js
@@ -1,96 +1,64 @@
-/*
-fetch("https://autenticador.secullum.com.br/Token?grant_type=password&username=desenvolvimento05.onnetmais@gmail.com&password=D123&client_id=3", {
-  method: 'POST',
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    const token = data.access_token;
-    console.log(token);
-    
-// URL do endpoint protegido
-const protectedUrl = "https://pontowebintegracaoexterna.secullum.com.br/IntegracaoExterna/Funcionarios";
-
-// Cabeçalho de autenticação com o Bearer Token
-const headers = {
-  "Authorization": `Bearer ${token}`
-};
-
-// Fazer a solicitação GET ao endpoint protegido
-return fetch(protectedUrl, { headers });
-})
-.then((response) => {
-if (response.ok) {
-  console.log(response.json());
-  return response.json();
-} else {
-  throw new Error("Erro na solicitação do endpoint protegido");
-}
-})
-.then((data) => {
-// Você pode lidar com a resposta do endpoint protegido aqui
-})
-.catch((error) => {
-console.error(error);
-})
-  .catch(error => {
-    console.error(error);
-  });
-//Funcionando retorno do token
-*/
 document.addEventListener("DOMContentLoaded", function() {
+  const keysToShow = ["Nome", "Telefone", "Cidade", "Funcao", "Nascimento"];
 
-  const keysToShow = ["Nome", "Telefone", "CidadeId", "FuncaoId", "Nascimento"];
-
-
-fetch("https://autenticador.secullum.com.br/Token?grant_type=password&username=desenvolvimento05.onnetmais@gmail.com&password=D123&client_id=3", {
-  method: 'POST',
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
+  fetch("https://autenticador.secullum.com.br/Token?grant_type=password&username=desenvolvimento05.onnetmais@gmail.com&password=D123&client_id=3", {
+    method: 'POST',
   })
-  .then(data => {
-    const token = data.access_token;
-    console.log(token);
-    const protectedUrl = "https://pontowebintegracaoexterna.secullum.com.br/IntegracaoExterna/Funcionarios";
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const token = data.access_token;
+      console.log(token);
+      const protectedUrl = "https://pontowebintegracaoexterna.secullum.com.br/IntegracaoExterna/Funcionarios";
 
-    // Cabeçalho de autenticação com o Bearer Token
-    const headers = {
-      "Authorization": `Bearer ${token}`
-    };
-    return fetch(protectedUrl, { headers });
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Erro na solicitação do endpoint protegido");
-    }
-    return response.json();
-  })
-  .then(data => {
-            const tbody = document.querySelector('tbody');
+      // Cabeçalho de autenticação com o Bearer Token
+      const headers = {
+        "Authorization": `Bearer ${token}`
+      };
+      return fetch(protectedUrl, { headers });
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Erro na solicitação do endpoint protegido");
+      }
+      return response.json();
+    })
+    .then(data => {
+      const tbody = document.querySelector('tbody');
+      data.forEach(item => {
+        const row = tbody.insertRow();
 
-            data.forEach(item => {
-                const row = tbody.insertRow();
-
-                keysToShow.forEach(key => {
-                    const cell = row.insertCell();
-                    const text = document.createTextNode(item[key]);
-                    cell.appendChild(text);
-                });
+        keysToShow.forEach(key => {
+          if(!item.Demissao){
+          const cell = row.insertCell();
+          let text;
+            switch (key) {
+              case "Cidade":
+                text = document.createTextNode(item.Cidade ? item.Cidade.Descricao : "-");
+                break;
+              case "Funcao":
+                text = document.createTextNode(item.Funcao ? item.Funcao.Descricao : "-");
+                break;
+              case "Telefone":
+                text = document.createTextNode(item.Telefone ? item.Telefone : item.Celular);
+              default:
+                text = document.createTextNode(item[key] || "-");
+                break;
+            }
+  
+            cell.appendChild(text);
+          }else{
             
-        })
-        .catch(error => {
-            console.error("Erro ao buscar dados da API:", error);
+          }
+          
         });
-  })
-  .catch(error => {
-    console.error(error);
-  });
+      })
+    })
+    .catch(error => {
+      console.error("Erro ao buscar dados da API:", error);
+    });
 });
